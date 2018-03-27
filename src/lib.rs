a6e9c1af325bbea795fa2a67dd7d716aa4bd66f0
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2018-03-27 23:04:45 +0300

    make module public

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
-mod ruuvitag {
-use std::collections::HashMap;
+pub mod ruuvitag {
+    use std::collections::HashMap;
     #[derive(Debug, PartialEq, Eq)]
     pub enum TagError {
         UnknownManufacturerId,
@@ -19,7 +19,7 @@ use std::collections::HashMap;
     }
 
     #[derive(Debug)]
-    struct Acceleration {
+    pub struct Acceleration {
         x: i16,
         y: i16,
         z: i16,
@@ -58,43 +58,47 @@ use std::collections::HashMap;
         (integer as f64 + decimal)
     }
 }
-#[test]
-fn parse_packet() {
-    let mut packet: HashMap<u16, Vec<u8>> = HashMap::new();
-    packet.insert(
-        1177,
-        vec![3, 172, 5, 31, 192, 7, 2, 215, 2, 223, 255, 247, 11, 95],
-    );
-    assert_eq!(packet.len(), 1);
-    let tag_data = Tag::new(packet).unwrap();
-    assert_eq!(tag_data.id, 3);
-    assert_eq!(tag_data.humidity, 86 as f64);
-    assert_eq!(tag_data.temperature, 5.31 as f64);
-    assert_eq!(tag_data.pressure, 99159);
-    assert_eq!(tag_data.acceleration.x, 0x2d7);
-    assert_eq!(tag_data.acceleration.y, 0x2df);
-    assert_eq!(tag_data.acceleration.z, 0xfff7);
-    assert_eq!(tag_data.battery_voltage, 2911);
-}
+mod tests {
+    // add code here
+    #[test]
+    fn parse_packet() {
+        let mut packet: HashMap<u16, Vec<u8>> = HashMap::new();
+        packet.insert(
+            1177,
+            vec![3, 172, 5, 31, 192, 7, 2, 215, 2, 223, 255, 247, 11, 95],
+        );
+        assert_eq!(packet.len(), 1);
+        let tag_data = Tag::new(packet).unwrap();
+        assert_eq!(tag_data.id, 3);
+        assert_eq!(tag_data.humidity, 86 as f64);
+        assert_eq!(tag_data.temperature, 5.31 as f64);
+        assert_eq!(tag_data.pressure, 99159);
+        assert_eq!(tag_data.acceleration.x, 0x2d7);
+        assert_eq!(tag_data.acceleration.y, 0x2df);
+        assert_eq!(tag_data.acceleration.z, 0xfff7);
+        assert_eq!(tag_data.battery_voltage, 2911);
+    }
 
-#[test]
-fn invalid_manufacturer_id() {
-    let mut packet: HashMap<u16, Vec<u8>> = HashMap::new();
-    packet.insert(
-        0x123,
-        vec![3, 172, 5, 31, 192, 7, 2, 215, 2, 223, 255, 247, 11, 95],
-    );
-    assert_eq!(Tag::new(packet).is_err(), true);
-    //TODO:
-    //assert_eq!(Tag::new(packet), TagError::UnknownManufacturerId);
-}
+    #[test]
+    fn invalid_manufacturer_id() {
+        let mut packet: HashMap<u16, Vec<u8>> = HashMap::new();
+        packet.insert(
+            0x123,
+            vec![3, 172, 5, 31, 192, 7, 2, 215, 2, 223, 255, 247, 11, 95],
+        );
+        assert_eq!(Tag::new(packet).is_err(), true);
+        //TODO:
+        //assert_eq!(Tag::new(packet), TagError::UnknownManufacturerId);
+    }
 
-#[test]
-fn minus_degrees() {
-    let mut packet: HashMap<u16, Vec<u8>> = HashMap::new();
-    packet.insert(
-        1177,
-        vec![3, 111, 133, 94, 198, 212, 2, 197, 2, 224, 255, 255, 11, 95],
-    );
-    assert_eq!(Tag::new(packet).unwrap().temperature, -5.9399999999999995)
+    #[test]
+    fn minus_degrees() {
+        let mut packet: HashMap<u16, Vec<u8>> = HashMap::new();
+        packet.insert(
+            1177,
+            vec![3, 111, 133, 94, 198, 212, 2, 197, 2, 224, 255, 255, 11, 95],
+        );
+        assert_eq!(Tag::new(packet).unwrap().temperature, -5.9399999999999995)
+    }
+
 }
