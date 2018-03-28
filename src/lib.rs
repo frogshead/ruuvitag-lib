4a4480a3b769a2d1b44d25cc83b06cebe627444d
Mikko Viitamäki <mikko.viitamaki+github@gmail.com>
2018-03-28 20:16:17 +0300

    still trying

--- src/lib.rs
+++ src/lib.rs
@@ -1,67 +1,69 @@
+pub mod tag {
+
 #![allow(dead_code)]
-pub mod ruuvitag {
 use std::collections::HashMap;
 
-    #[derive(Debug, PartialEq, Eq)]
-    pub enum TagError {
-        UnknownManufacturerId,
-        UnknownPacketSpecification,
-    }
+#[derive(Debug, PartialEq, Eq)]
+pub enum TagError {
+    UnknownManufacturerId,
+    UnknownPacketSpecification,
+}
 
-    #[derive(Debug)]
-    pub struct Tag {
-        pub id: u8,
-        pub humidity: f64,
-        pub temperature: f64,
-        pub pressure: u32,
-        pub acceleration: Acceleration,
-        pub battery_voltage: u16,
-    }
+#[derive(Debug)]
+pub struct Tag {
+    pub id: u8,
+    pub humidity: f64,
+    pub temperature: f64,
+    pub pressure: u32,
+    pub acceleration: Acceleration,
+    pub battery_voltage: u16,
+}
 
-    #[derive(Debug)]
-    pub struct Acceleration {
-        pub x: i16,
-        pub y: i16,
-        pub z: i16,
-    }
+#[derive(Debug)]
+pub struct Acceleration {
+    pub x: i16,
+    pub y: i16,
+    pub z: i16,
+}
 
-    impl Tag {
-        pub fn new(data: HashMap<u16, Vec<u8>>) -> Result<Tag, TagError> {
-            if !data.contains_key(&0x0499) {
-                return Err(TagError::UnknownManufacturerId);
-            }
-
-            let values = data.get(&0x0499).unwrap();
-            let tag = Tag {
-                id: values[0],
-                humidity: (values[1] / 2) as f64,
-
-                //temperature: if ( values[2] & 0x80 == 0x80) { (-1 * (values[2] & 0x7f) as i8) as f64} else { (values[2] & 0x7f) as f64 } + ((values[3] as f64 * 0.01)),
-                temperature: parse_temperature(values[2], values[3]),
-                pressure: (((values[4] as u32) << 8) | values[5] as u32) + 50000,
-                acceleration: Acceleration {
-                    x: (((values[6] as i16) << 8) | values[7] as i16),
-                    y: (((values[8] as i16) << 8) | values[9] as i16),
-                    z: (((values[10] as i16) << 8) | values[11] as i16),
-                },
-                battery_voltage: (((values[12] as u16) << 8) | values[13] as u16),
-            };
-            Ok(tag)
+impl Tag {
+    pub fn new(data: HashMap<u16, Vec<u8>>) -> Result<Tag, TagError> {
+        if !data.contains_key(&0x0499) {
+            return Err(TagError::UnknownManufacturerId);
         }
-    }
-    fn parse_temperature(t_msb: u8, t_lsb: u8) -> f64 {
-        let integer: u8 = 0x7F & t_msb;
-        let decimal: f64 = t_lsb as f64 / 100f64;
-        if 0x80 & t_msb == 0x80 {
-            return -1.00 * (integer as f64 + decimal);
-        }
-        (integer as f64 + decimal)
+
+        let values = data.get(&0x0499).unwrap();
+        let tag = Tag {
+            id: values[0],
+            humidity: (values[1] / 2) as f64,
+
+            //temperature: if ( values[2] & 0x80 == 0x80) { (-1 * (values[2] & 0x7f) as i8) as f64} else { (values[2] & 0x7f) as f64 } + ((values[3] as f64 * 0.01)),
+            temperature: parse_temperature(values[2], values[3]),
+            pressure: (((values[4] as u32) << 8) | values[5] as u32) + 50000,
+            acceleration: Acceleration {
+                x: (((values[6] as i16) << 8) | values[7] as i16),
+                y: (((values[8] as i16) << 8) | values[9] as i16),
+                z: (((values[10] as i16) << 8) | values[11] as i16),
+            },
+            battery_voltage: (((values[12] as u16) << 8) | values[13] as u16),
+        };
+        Ok(tag)
     }
 }
+fn parse_temperature(t_msb: u8, t_lsb: u8) -> f64 {
+    let integer: u8 = 0x7F & t_msb;
+    let decimal: f64 = t_lsb as f64 / 100f64;
+    if 0x80 & t_msb == 0x80 {
+        return -1.00 * (integer as f64 + decimal);
+    }
+    (integer as f64 + decimal)
+}
+}
 mod tests {
- #![allow(dead_code)]   
-use ruuvitag;
-use std::collections::HashMap;
+    #![allow(dead_code)]
+    
+    use std::collections::HashMap;
+    use super::*;
     // add code here
     #[test]
     fn parse_packet() {
